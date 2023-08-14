599c131faf78edae9192f89b2a460491ad6e55f0
DucKyWay <niji2549@gmail.com>
2023-08-14 20:56:14 +0700

    remove ;

--- python.py
+++ python.py
@@ -1,14 +1,14 @@
 def get_box_area(width, length, height):
     area = width * length * height
-    print(area);
+    print(area)
 
 def get_triangle_area(base, height):
     area = 1/2 * base * height
-    print(area);
+    print(area)
 
 def get_circle_area(radius):
     area = 22/7 * pow(radius, 2)
-    print(area);
+    print(area)
 
 triangle_area_1 = get_triangle_area(4, 4)
 circle_area_1 = get_circle_area(7)
